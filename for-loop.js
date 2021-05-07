4de6c6d7b4bc9c020797c64f4fcc3cf2eed142b8
nv_twistt <mtamkee@icloud.com>
2021-05-07 22:30:52 +0000

    all files

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+var total = 0;
+var limit = 10;
+var i = 0;
+for(i = 0; i<limit; i++){
+    total += i;
+}
+console.log(total);
---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit
+fruit = "orange"
+var len = fruit.length
+if(len > 5){
+    console.log("The fruit name has more than five characters.")
+}
+else{
+    console.log("The fruit name has five characters or lesss.")
+}
