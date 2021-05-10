ab961c69d598959e482355f72a824057c5ab11ac
nv_twistt <mtamkee@icloud.com>
2021-05-09 23:15:30 -0700

    all commit

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+const math = function(firstNum, secondNum, thirdNum){
+  return (secondNum * thirdNum) + firstNum;
+}
+console.log(math(53, 61,67));
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets = ['cat', 'dog', 'rat'];
+for(var i = 0; i <= pets.length - 1; i++){
+  pets[i] = pets[i] + 's';
+}
+console.log(pets);
